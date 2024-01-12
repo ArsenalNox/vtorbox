ce6c5dd02f250e67b88c0afe0a73192eeb4aeee5
Dyushes <nohtimore@gmail.com>
2024-01-12 13:42:52 +0500

    Update couriers.py
    - Разметка эндпоинтов

--- app/routers/couriers.py
+++ app/routers/couriers.py
@@ -16,3 +16,19 @@ async def create_new_courier(courier_data: CourierCreationValidator):
 @router.get('/courier/{courier_tg_id}', tags=["couriers"])
 async def get_courier_by_id():
     pass
+
+
+@router.post('/courier/order/{order_id}/accept', tags=["couriers", "orders"])
+async def accept_order_by_courier():
+    """
+    Принять заявку курьером
+    """
+    pass
+
+
+@router.post('/courier/order/{order_id}/comment', tags=["couriers", "orders"])
+async def post_order_comment_by_courier():
+    """
+    Оставить комментарий к заявке курьером
+    """
+    pass
