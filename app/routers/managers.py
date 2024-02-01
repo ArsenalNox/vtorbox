09db5561d638adc3bf5b84e12326d8be4294c761
Dyushes <nohtimore@gmail.com>
2024-02-01 19:50:58 +0500

    Update managers.py

--- app/routers/managers.py
+++ app/routers/managers.py
@@ -1,3 +1,53 @@
 """
 Эндпоинты для менеждеров
 """
+
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status, Security
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm, SecurityScopes
+
+from ..models import (
+    engine, 
+    Session, 
+    OrderStatuses,
+)
+
+from ..validators import (
+    UserLogin as UserLoginSchema,
+    StatusOut
+)
+
+from ..auth import (
+    oauth2_scheme, 
+    pwd_context, 
+    get_password_hash, 
+    verify_password, 
+    create_access_token,
+    get_current_active_user,
+    get_current_user,
+    ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+)
+
+import os, uuid
+from dotenv import load_dotenv
+
+
+load_dotenv()
+router = APIRouter()
+
+
+@router.post('/managers/add/{user_id}')
+async def add_manager_role_to_user():
+    """
+    Добавить роль менеджера пользователю
+    """
+    pass
+
+
+@router.get("/managers")
+async def get_managers():
+    """
+    Получить менеджеров
+    """
+    pass
