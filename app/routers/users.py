0866501a644779140f1ddcd45fbc4bcf6b506b50
Dyushes <nohtimore@gmail.com>
2024-02-01 19:47:51 +0500

    Update users.py
    обновил получение списка пользователей:
    - Добавил правильную пагинацию при применении фильтров
    - Корректную фильтрацию по ролям
    - Вывод ролей в выходных данных
    - Глобальный count для пагинации на клиенте
    
    signup:
    - При логине с ролью тг бота токен не истекает
    
    добавил импорт клиентов с созданием их заявки
    
    Эндпоинт на проверку ролей пользователей
    
    Эндпоинт на получение всех данных о пользователе админом

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,18 +7,27 @@ CRUD с пользователями
 """
 
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status, Security
+from fastapi import (
+    APIRouter, 
+    Depends, 
+    HTTPException, 
+    status, 
+    Security, 
+    File, 
+    UploadFile)
+
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm, SecurityScopes
 
 from datetime import datetime
 
-from sqlalchemy import desc, asc
+from sqlalchemy import desc, asc, not_, or_
+from app import Tags
 
 from ..models import (
     Users, Orders, Session, engine, Roles, Permissions, 
-    Address, UsersAddress, BoxTypes, OrderStatuses,
-    ROLE_CUSTOMER_NAME
+    Address, UsersAddress, BoxTypes, OrderStatusHistory,
+    OrderStatuses, ROLE_CUSTOMER_NAME, ROLE_TELEGRAM_BOT_NAME
     )
 
 from ..auth import (
@@ -40,7 +49,7 @@ from ..validators import (
     UserOut, OrderOut
 )
 
-from app.utils import is_valid_uuid
+from app.utils import is_valid_uuid, get_lang_long_from_text_addres
 
 from passlib.context import CryptContext
 from datetime import timedelta
@@ -51,6 +60,7 @@ from dotenv import load_dotenv
 
 import os, uuid, re, json
 
+import openpyxl as xl 
 
 load_dotenv()
 router = APIRouter()
@@ -80,6 +90,7 @@ router = APIRouter()
 async def get_all_users(
         current_user: Annotated[UserLoginSchema, Security(get_current_user, scopes=["manager"])],
         role_name: str = None,
+        only_bot_users: bool = False,
         non_deleted: bool = True,
         #Применимо для клиентов
         with_orders: bool = False,
@@ -95,32 +106,41 @@ async def get_all_users(
     Получение пользователей по фильтру
     """
 
-    #DONE: Пагинация
     with Session(engine, expire_on_commit=False) as session:
         
-        query = session.query(Users,Roles,Permissions).\
-                    join(Roles, Permissions.role_id == Roles.id).\
-                    join(Users, Permissions.user_id == Users.id)
+        query = session.query(Users)
+
+        roles_query = session.query(Roles).all()
+        roles_list = [role.role_name for role in roles_query]
 
         if not show_deleted:
-
             query = query.filter(Users.deleted_at == None)
 
-        if role_name:
-            print(role_name)
-            # role_name = f"%{role_name}%"
-            # role_query = session.query(Roles).filter(Roles.role_name.like(role_name)).first()
-            query = query.filter(Roles.role_name.like(f"%{role_name}%"))
-            print(query)
+        if only_bot_users:
+            query = query.filter(or_(Users.link_code == None, not_(Users.telegram_username == None)))
 
-        users = query.offset(page  * limit).limit(limit).all()
+        if (role_name != None) and (role_name in roles_list):
+            roles_user_query = session.query(Users.id).\
+                join(Permissions, Permissions.user_id == Users.id).\
+                join(Roles, Roles.id == Permissions.role_id).\
+                where(Roles.role_name == role_name).subquery()
+
+            query = query.filter(Users.id.in_(roles_user_query))
+
+        global_user_count = query.count()
+        users = query.order_by(asc(Users.date_created)).offset(page  * limit).limit(limit).all()
         # users = session.query(Users, Roles, Permissions).filter_by(*filters).offset(page  * limit).limit(limit).all()
 
         total = len(users)
         data = []
         for user in users:
 
-            user_data = UserOut(**user[0].__dict__)
+            user_data = UserOut(**user.__dict__)
+
+
+            scopes_query = session.query(Permissions, Roles.role_name).filter_by(user_id=user.id).join(Roles).all()
+
+            user_data.roles = [role.role_name for role in scopes_query]
 
             #TODO: Фильтр по статусу заявки
             if with_orders:
@@ -128,7 +148,7 @@ async def get_all_users(
                     join(Address, Address.id == Orders.address_id).\
                     join(BoxTypes, BoxTypes.id == Orders.box_type_id).\
                     join(OrderStatuses, OrderStatuses.id == Orders.status).\
-                    where(Orders.from_user == user[0].id).order_by(asc(Orders.date_created)).all()
+                    where(Orders.from_user == user.id).order_by(asc(Orders.date_created)).all()
                 
                 user_data.orders = []
                 for order in orders:
@@ -157,16 +177,19 @@ async def get_all_users(
 
         return {
             "count": total,
+            "global_count": global_user_count,
             "data": data
         }
 
     return []
 
 
+
 @router.post('/users', tags=["admins"])
 async def create_user(
     current_user: Annotated[UserLoginSchema, Security(get_current_user, scopes=["manager"])],
-    new_user_data: UserCreationData
+    new_user_data: UserCreationData,
+    send_email: bool = True
 ):
     """
     Ручное создание нового пользователя
@@ -211,6 +234,9 @@ async def create_user(
                 session.add(user_role)
 
         session.commit()
+
+        #TODO: Отправка приглашения на почту
+
         return new_user
 
 
@@ -282,7 +308,7 @@ async def update_user_data(
     return 
 
 
-@router.put('/users', tags=["user"])
+@router.put('/users', tags=[Tags.users])
 async def update_user_self_data(
     current_user: Annotated[UserLoginSchema, Security(get_current_user, scopes=["customer"])],
     new_user_data: UserUpdateData
@@ -292,7 +318,8 @@ async def update_user_self_data(
     """
 
 
-@router.get('/users/bot_linked', tags=['users'])
+
+@router.get('/users/bot_linked', tags=[Tags.bot, Tags.users])
 async def get_bot_users():
     """
     Получение пользователей тг бота, или пользователей сайта связавших профиль с тг
@@ -302,7 +329,7 @@ async def get_bot_users():
         return bot_users
 
 
-@router.post('/users/signup', tags=["users"])
+@router.post('/users/signup', tags=[Tags.users])
 async def signup(signup_data: UserSignUpSchema):
     """
     Регистрация клиента
@@ -362,8 +389,11 @@ async def login(login_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         scopes_query = session.query(Permissions, Roles.role_name).filter_by(user_id=query.id).join(Roles).all()
 
         scopes = [role.role_name for role in scopes_query]
-        print(scopes)
+
         expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+        if ROLE_TELEGRAM_BOT_NAME in scopes:
+            expires = None
+
         token = create_access_token(
             data={
                 "sub": login_data.username,
@@ -378,7 +408,7 @@ async def login(login_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         })
 
 
-@router.post("/users/me", tags=["users"], responses={
+@router.get("/users/me", tags=["users"], responses={
     200: {
         "description": "Получение информации об авторизованном пользователе (себе)",
         "content": {
@@ -426,16 +456,182 @@ async def get_current_user_info(
 
     with Session(engine, expire_on_commit=False) as session:
         query = session.query(Users).filter_by(email=current_user.username).first()
+
         return {
             "user_data": query,
             "token_data": token_data
             }
 
 
-@router.post('/users/import_clients', tags=["admin"])
-async def import_clients():
+@router.get('/user/{user_id}', tags=[Tags.users, Tags.admins])
+async def get_user_data(
+    user_id: uuid.UUID,
+    current_user: Annotated[UserLoginSchema, Security(get_current_user, scopes=["admin"])],
+):
     """
-    Импорт клиентов из excel таблицы
+    Получить данные пользователя админом
     """
+    with Session(engine, expire_on_commit=False) as session:
+        user_query = session.query(Users).where(Users.id == user_id).first()
+        if not user_query:
+            return JSONResponse({
+                "message": "Not found"
+            }, status_code=404)
+        return user_query
+
+
+@router.get('/user/me', tags=[Tags.bot, Tags.users])
+async def get_user_info(
+    tg_id: int,
+    bot: Annotated[UserLoginSchema, Security(get_current_user, scopes=["bot"])]
+):
+
+    with Session(engine, expire_on_commit=False) as session:
+        query = session.query(Users).filter_by(email=current_user.username).first()
+
+        scopes_query = session.query(Permissions, Roles.role_name).filter_by(user_id=query.id).join(Roles).all()
+        scopes = [role.role_name for role in scopes_query]
+        return JSONResponse({
+            "user_data": query,
+            "scopes": scopes
+        })
     pass
 
+
+@router.get('/users/check', tags=[Tags.admins, Tags.users])
+async def check_user_roles(
+    current_user: Annotated[UserLoginSchema, Security(get_current_user, scopes=["admin"])]
+):
+    """
+    Проверить базу данных пользователей на пользователей без роли и добавить им роль customer
+    """
+    with Session(engine, expire_on_commit=False) as session:
+        user_qeury = session.query(Users).all()
+        for user in user_qeury:
+            scopes_query = session.query(Permissions, Roles.role_name).filter_by(user_id=user.id).join(Roles).all()
+            scopes = [role.role_name for role in scopes_query]
+            if len(scopes) < 1:
+                print(f"User {user.id} has no roles")
+                print(scopes)
+                if ROLE_CUSTOMER_NAME not in scopes:
+                    #TODO: Добавить роль
+                    user_role = Permissions(
+                        user_id = user.id,
+                        role_id = Roles.customer_role()
+                    )
+                    session.add(user_role)
+
+        session.commit()
+
+
+@router.post('/users/import/file', tags=["admins"])
+async def import_clients(file: UploadFile):
+    """
+    Импорт клиентов и заявки с историей из excel таблицы
+    """
+    with Session(engine, expire_on_commit=False) as session:
+    
+        wb_obj = xl.load_workbook(file.file)
+        sheet_obj = wb_obj.active
+
+        users = []
+
+        none_count = 0 
+        #импорт самих клиентов, адресов, тарифов, подписки, интервалов
+        for row in range(1, sheet_obj.max_row+1):
+            print(f"row number {row} ")
+        # for row in range(1, 10):
+            #3840 24
+            #print(sheet_obj.max_row, sheet_obj.max_column)
+            cell_obj = sheet_obj.cell(row, 1)
+
+            if cell_obj.value == None:
+                none_count +=1
+            else:
+                none_count = 0
+
+            if not re.match(r'[\d](.*)', str(cell_obj.value)):
+                if none_count > 30:                
+                    break
+                continue
+
+            # print(get_lang_long_from_text_addres(sheet_obj.cell(row,7).value))
+            try:
+                phone_number = int(sheet_obj.cell(row, 11).value)
+            except Exception as err:
+                continue
+
+            #TODO: useraddress
+            new_user = session.query(Users).filter_by(phone_number = str(phone_number)).first()
+            if not new_user:
+                new_user = Users(
+                    phone_number = int(sheet_obj.cell(row, 11).value),
+                    firstname = sheet_obj.cell(row, 9).value,
+                    link_code = str(uuid.uuid4())[:8]
+                )
+
+                session.add(new_user)
+                session.flush()
+                session.refresh(new_user)
+            
+            new_address = session.query(Address).filter_by(address = sheet_obj.cell(row,7).value).first()
+            if not new_address:
+                latitude, longitude = get_lang_long_from_text_addres(sheet_obj.cell(row,7).value)
+                if (latitude == None) or (longitude == None):
+                    print(f"No data found for address {sheet_obj.cell(row, 7).value}")
+                    continue
+
+                new_address = Address(
+                    region = sheet_obj.cell(row, 4).value,
+                    district = sheet_obj.cell(row, 5).value,
+                    distance_from_mkad = sheet_obj.cell(row, 6).value,
+                    point_on_map = sheet_obj.cell(row, 8).value,
+                    address = sheet_obj.cell(row, 7).value,
+                    detail = sheet_obj.cell(row, 10).value,
+                    main = True,
+                    longitude = longitude,
+                    latitude = latitude
+                )
+
+                session.add(new_address)
+                session.flush()
+                session.refresh(new_address)
+
+            user_role = Permissions(
+                user_id = new_user.id,
+                role_id = Roles.customer_role()
+            )
+
+            count = session.query(Orders.id).where(Orders.from_user == new_user.id).count()
+            new_order = Orders(
+                from_user   = new_user.id,
+                address_id  = new_address.id,
+                box_type_id = BoxTypes.test_type().id,
+                box_count   = 1,
+                status      = OrderStatuses.status_default().id,
+                date_created = datetime.now(),
+                user_order_num = count + 1
+            )
+
+            session.add(new_order)
+            session.add(user_role)
+            session.flush()
+            session.refresh(new_order)
+
+            status_update = OrderStatusHistory(
+                order_id = new_order.id,
+                status_id = OrderStatuses.status_default().id,
+                date = datetime.now()
+            )
+
+            user_address = UsersAddress(
+                user_id=new_user.id,
+                address_id=new_address.id,
+            )
+
+            session.add(status_update)
+            session.add(new_order)
+
+            session.commit()
+
+    return {"filename": file.filename}
