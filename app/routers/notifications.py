7f765d6bfd2bdbbd8f451dc766f282a527ccda0c
Dyushes <nohtimore@gmail.com>
2024-03-18 12:41:58 +0500

    Update notifications.py

--- app/routers/notifications.py
+++ app/routers/notifications.py
@@ -1,3 +1,88 @@
 """
 Эндпоинт по уведомлениям
 """
+
+import requests
+import os, uuid
+import re 
+import math
+
+from typing import Annotated, List, Tuple, Dict, Optional
+from fastapi.responses import JSONResponse
+
+import datetime as dt
+from datetime import datetime, timedelta
+
+
+from app import CODER_KEY, CODER_SETTINGS
+
+from app.auth import (
+    oauth2_scheme, 
+    get_current_user
+)
+
+from app.validators import (
+    LinkClientWithPromocodeFromTG as UserLinkData,
+    Address as AddressValidator,
+    AddressUpdate as AddressUpdateValidator,
+    UserLogin as UserLoginSchema,
+    AddressSchedule, CreateUserData, UpdateUserDataFromTG, AddressOut,
+    RegionOut, AddressDaysWork, UserOut, RouteOut
+)
+
+from app import Tags
+
+from fastapi import APIRouter, Body, Security, Query
+from fastapi.responses import JSONResponse
+
+from calendar import monthrange
+from uuid import UUID
+
+from sqlalchemy import desc, asc, desc
+
+from app.validators import (
+    Order as OrderValidator,
+    UserLogin as UserLoginSchema,
+    OrderOut,
+    OrderUpdate
+)
+
+from ..auth import (
+    get_current_user
+)
+
+from app.models import (
+    Users, Session, engine, UsersAddress, 
+    Address, IntervalStatuses, Roles, Permissions, Regions, WEEK_DAYS_WORK_STR_LIST
+    )
+
+
+from app.models import (
+    engine, Orders, Users, Session, 
+    Address, UsersAddress, BoxTypes,
+    OrderStatuses, OrderStatusHistory,
+    ORDER_STATUS_DELETED, ORDER_STATUS_AWAITING_CONFIRMATION,
+    IntervalStatuses, ROLE_ADMIN_NAME, ROLE_COURIER_NAME,
+    Routes, RoutesOrders
+    )
+
+
+router = APIRouter()
+
+
+@router.get("/my")
+async def get_my_notifications():
+    """
+    Получить уведомления текущего пользователя
+    """
+    pass
+
+
+@router.get('/user')
+async def get_user_notifications(
+    user_id: int|UUID = None
+):
+    """
+    Получить уведомления пользователя по айди
+    """
+    pass
