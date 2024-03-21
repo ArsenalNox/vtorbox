9cef06157e68bccd981670b00045c3cbaf63d91c
Dyushes <nohtimore@gmail.com>
2024-03-21 08:27:36 +0500

    Update utils.py
    Функция для отправки сообщений через тг бота

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import uuid, os, requests
-from app import CODER_KEY, CODER_SETTINGS
+from app import CODER_KEY, CODER_SETTINGS, BOT_TOKEN
 
+token = BOT_TOKEN
 
 def is_valid_uuid(value):
     """
@@ -31,3 +32,34 @@ def get_lang_long_from_text_addres(address):
 
     except Exception as err: 
         return None, None
+
+
+def send_message_through_bot(receipient_id:int, message, btn=None):
+    """
+    Отправить сообщение пользователю через тг бота 
+    """
+
+    method = 'sendMessage'
+
+    if btn:
+        b = {
+            "chat_id" : receipient_id,
+            "text" : message,
+            "parse_mode" : "html",
+            "reply_markup" : btn
+        }
+    else:
+        b = {
+            "chat_id" : receipient_id,
+            "text" : message,
+            "parse_mode" : "html"
+        }
+
+    try:
+        test_request = requests.post(
+            url='https://api.telegram.org/bot{0}/{1}'.format(token, method), json=b
+        ).json()
+        print(test_request)
+
+    except Exception as err:
+        print(err)
