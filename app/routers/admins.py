e4390a3b24eb38c44bdec3985c15a5fc75eebd3e
Dyushes <nohtimore@gmail.com>
2024-05-08 12:58:47 +0500

    Update admins.py
    - Получение статусов с разрешёнными к переходу статусами
    - Редактирование статуса и его сетки

--- app/routers/admins.py
+++ app/routers/admins.py
@@ -6,17 +6,20 @@
 # чтобы давать их на выбор клиентам, админ может вкл/выкл какие-то шлюзы, управлять их настройками)
 
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status, Security
+from fastapi import APIRouter, Depends, HTTPException, status, Security, Query 
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm, SecurityScopes
+from fastapi.encoders import jsonable_encoder 
 
 from uuid import UUID
 
+from typing import Annotated, List, Tuple, Dict, Optional
 from app.models import (
     engine, 
     Session, 
     OrderStatuses,
-    Roles, Permissions
+    Roles, Permissions, OrderStatusesAllowFromList,
+    OrderStatusesAllowToList
 )
 
 from app.validators import (
@@ -52,8 +55,15 @@ async def get_all_statueses(
     """
     with Session(engine, expire_on_commit=False) as session:
         statuses = session.query(OrderStatuses).all()
-        statuses_list = [StatusOut(**status.__dict__) for status in statuses]
-        return statuses_list
+
+        return_data = []
+
+        for status in statuses:
+            status.allow_to_list
+            status.allow_from_list
+            return_data.append(status)
+
+        return return_data
 
 
 @router.get('/statuses/name', tags=['statuses'])
@@ -70,14 +80,18 @@ async def get_status_info_by_name(
         return status
 
 
-@router.put('/statuses/{status_id}', tags=['admins'])
+@router.put('/statuses/{status_id}', tags=[Tags.statuses])
 async def change_status(
     current_user: Annotated[UserLoginSchema, Security(get_current_user)],
     status_id: UUID,
-    allow_messages: bool
+    allow_messages: bool,
+    status_allow_to: List[UUID] = Query(None),
+    status_allow_from: List[UUID] = Query(None),
 ):
     """
     - **allow_messages**: вкл/выкл отправку сообщения при изменении статуса на указанный
+    - **status_allow_to**: - лист айди статусов, к которым статус может перейти
+    - **status_allow_from**: - лист айди статусов, от которых статус может перейти
     """
     with Session(engine, expire_on_commit=False) as session:
         status = session.query(OrderStatuses).filter(OrderStatuses.id == status_id).first()
@@ -88,8 +102,28 @@ async def change_status(
 
         status.message_on_update = allow_messages
         session.add(status)
-        session.commit()
 
+        if status_allow_to:
+            delete_query = session.query(OrderStatusesAllowToList).filter_by(status_id = status_id).delete()
+
+            for status_to_id in status_allow_to:
+                new_allow_to = OrderStatusesAllowToList(
+                    status_id = status_id,
+                    second_status_id = status_to_id
+                )
+                session.add(new_allow_to)
+
+        if status_allow_from:
+            delete_query = session.query(OrderStatusesAllowFromList).filter_by(status_id = status_id).delete()
+
+            for status_from_id in status_allow_from:
+                new_allow_from = OrderStatusesAllowFromList(
+                    status_id = status_id,
+                    second_status_id = status_from_id
+                )
+                session.add(new_allow_from)
+
+        session.commit()
         return status
 
 
@@ -100,7 +134,6 @@ async def get_avaliable_roles(
     """
     Получение списка ролей
     """
-
     with Session(engine, expire_on_commit=False) as session:
         roles_query = session.query(Roles).all()
         return_data = []
@@ -124,11 +157,3 @@ async def get_avaliable_roles(
             return_data.append(role)
 
         return return_data
-
-
-@router.get("/stats", tags=[Tags.admins])
-async def get_statistics():
-    """
-    получить статистику использования проекта
-    """
-    pass
