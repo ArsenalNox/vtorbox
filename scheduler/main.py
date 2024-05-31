f4922ece91c4f6be84722fd6075f7c9bd39ef7ad
Dyushes <59665801+ArsenalNox@users.noreply.github.com>
2024-06-01 01:05:30 +0500

    Update main.py

--- scheduler/main.py
+++ scheduler/main.py
@@ -95,23 +95,19 @@ def tirgger_payment_check(payment_id):
 
 #TODO: Получение настройки вкд\выкл генерации пула
 def trigger_poll_generation():
-    loggin.info('Generating poll')
     request = s.get(f'{api_url}/process_orders')
 
 
 #TODO: Получение настройки вкд\выкл генерации маршрутов
 def trigger_route_generation():
-    loggin.info('Generating routes')
     request = s.get(f'{api_url}/routes/generate?group_by=regions&write_after_generation=true')
 
 
 def check_intervals():
-    loggin.info('Checking intevals')
     request = s.get(f'{api_url}/check-intervals')
 
 
 def resend_notify():
-    loggin.info('Resending notifycations')
     request = s.get(f'{api_url}/resend-notify')
 
 
