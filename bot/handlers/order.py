5f2dfa6ac5f7681b7e9012bc8d0a143d0566b87a
macbook <romes57@gmail.com>
2024-06-05 18:08:02 +0300

    add accept/deny payment button

--- bot/handlers/order.py
+++ bot/handlers/order.py
@@ -438,7 +438,6 @@ class OrderHandler(Handler):
             await state.update_data(msg=callback.message.message_id)
             data = await state.get_data()
             flag = data.get('flag')
-            await callback.message.answer(f'payment {flag}')
             if data.get('order_msg'):
                 await callback.bot.edit_message_reply_markup(
                     chat_id=data.get('chat_id'),
@@ -522,7 +521,6 @@ class OrderHandler(Handler):
         @self.router.callback_query(F.data.startswith('accept_deny_payment'))
         async def accept_deny_payment(callback: CallbackQuery, state: FSMContext):
             flag = callback.data.split('_')[-2]
-            await callback.answer(f'callback {flag}')
             order_id = callback.data.split('_')[-1]
             if flag == 'False':
                 msg = await callback.bot.edit_message_reply_markup(
