cf3444f825f3a3295e740fe54c74720a01147ae3
Dyushes <nohtimore@gmail.com>
2024-07-10 21:21:45 +0500

    Отправка сообщения об успешной оплате

--- app/models.py
+++ app/models.py
@@ -1199,7 +1199,7 @@ class Payments(Base):
                 payment_query.status = response.json()['Status']
 
                 if r_data['Success'] and r_data['Status'] == "CONFIRMED":
-                    payment_query.order.update_status(OrderStatuses.status_payed().id)
+                    payment_query.order.update_status(OrderStatuses.status_payed().id, send_message=True)
 
                 session.commit()
             
