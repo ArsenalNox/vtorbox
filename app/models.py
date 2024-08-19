79730510a59e5534b4d2ea4c0f6bfc7815c56110
Dyushes <nohtimore@gmail.com>
2024-08-19 12:10:45 +0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -99,8 +99,17 @@ def order_order_num():
     получить кол-во заявок в таблице (учитывая удалённые)
     """
     with Session(engine, expire_on_commit=False) as session:
-        count = session.query(Orders.id).count()
-        return count + 1
+        count_global = session.query(func.max(Orders.order_num)).first()[0]+1
+        #TODO: Переписать проверку на существующий order_num
+        pre_check_count = session.query(Orders).filter(Orders.order_num == count_global+1).first()
+        if pre_check_count:
+            for i in range(1, 200):
+                post_check_count = session.query(Orders).filter(Orders.order_num == count_global+i).first()
+                if not post_check_count:
+                    logger.info(f"order num {count_global+i} is free")
+                    print(f"order num {count_global+i} is free")
+                    return count_global+i  
+        return count_global
 
 
 def generate_route_short_name()->str:
