1e94ddd82dbb78cfb3621c6c5da15618cedbb159
Dyushes <nohtimore@gmail.com>
2024-08-30 20:02:51 +0500

    подготовки к переносу

--- app/tests/test_07_automation.py
+++ app/tests/test_07_automation.py
@@ -2,6 +2,7 @@ from app.tests.test_01_before_main import client, test_globals
 
 
 def test_order_creation_by_interval():
+    #Сделать регион со всеми днями, сделать адресс со всеми днями
     pass
 
 
@@ -10,4 +11,9 @@ def test_order_poll_generation():
 
 
 def test_route_generation():
+    pass
+
+
+def test_resend_notification():
+    #TODO:???
     pass
